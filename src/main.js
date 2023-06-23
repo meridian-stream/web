947fbd07944c8818c7fa41e712b89cf886ad1b9d
Sam Littler <samlittler57@gmail.com>
2023-06-23 14:35:55 +0100

    Change google tag to separate data

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Router/Router'
-
 import './assets/main.scss'
-
 import 'bootstrap';
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
